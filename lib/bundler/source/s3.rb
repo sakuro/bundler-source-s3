338f3803071aeb1692a1238c8663d5e47df67ea8
OZAWA Sakuro <sakuro@2238.club>
2018-10-07 19:06:20 +0900

    $ bundle gem bundler-source-s3

---
+++ lib/bundler/source/s3.rb
@@ -0,0 +1,9 @@
+require "bundler/source/s3/version"
+
+module Bundler
+  module Source
+    module S3
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/bundler/source/s3/version.rb
@@ -0,0 +1,7 @@
+module Bundler
+  module Source
+    module S3
+      VERSION = "0.1.0"
+    end
+  end
+end
