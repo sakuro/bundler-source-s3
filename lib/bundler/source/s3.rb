cd35171491f47fc188f74a20b04c9e61647e3bdf
OZAWA Sakuro <sakuro@2238.club>
2018-10-08 14:04:29 +0900

    Move registration of source plugin to the definition itself

--- lib/bundler/source/s3.rb
+++ lib/bundler/source/s3.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
+require 'bundler/plugin/api'
+require 'bundler/source'
+
 require 'bundler/source/s3/version'
 require 'bundler/source/s3/fetcher'
+
 require 'aws-sdk-s3'
 
 module Bundler
   class Source
     class S3
-      # Your code goes here...
+      Bundler::Plugin::API.source('s3', self)
     end
   end
 end
--- plugins.rb
+++ plugins.rb
@@ -1,5 +1,3 @@
 # frozen_string_literal: true
 
 require 'bundler/source/s3'
-
-Bundler::Plugin::API.source('s3', Bundler::Source::S3)
